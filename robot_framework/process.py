3114fa24cfbb007aa9ab0619fb9995247df31709
kri-bak <kriba@aarhus.dk>
2025-08-22 08:34:21 +0200

    Added OO logging

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -19,13 +19,15 @@ def process(orchestrator_connection: OrchestratorConnection, queue_element: Queu
 
     credentials = orchestrator_connection.get_credential("Eflyt")
     webdriver = eflyt_login.login(credentials.username, credentials.password)
-
+    
     eflyt_search.search(webdriver, to_date=date.today(), case_state="Ubehandlet")
     cases = eflyt_search.extract_cases(webdriver)
+    oc.log_trace(f"Found {len(cases)} cases.")
 
     cases = filter_cases.filter_cases(cases)
 
     for case in cases:
+        orchestrator_connection.log_trace(f"Starting case {case}")
         eflyt_search.open_case(webdriver, case)
         handle_case(webdriver, orchestrator_connection)
 
@@ -38,6 +40,7 @@ def handle_case(webdriver: webdriver.Chrome, oc: OrchestratorConnection):
     move_date = rows[0].find_element(By.XPATH, "td[2]/a[1]").text
     first_habitant_is_applicant = len(applicants) == 1 and rows[0].find_element(By.XPATH, "td[2]/a[1]").text == "A"
     if not first_habitant_is_applicant:
+        oc.log_trace("Skipping case, more than one inhabitant")
         return
 
     letter_text = f"""Du har anmeldt udrejse af Danmark.
@@ -45,11 +48,13 @@ def handle_case(webdriver: webdriver.Chrome, oc: OrchestratorConnection):
 Vi har d. {date.today().strftime("%d-%m-%Y")} godkendt din anmodning om udrejse med virkning fra den {move_date}.
 """
     if not send_letter_to_anmelder(webdriver, letter_text):
+        oc.log_trace("Letter could not be sent.")
         return
 
     eflyt_case.approve_case(webdriver)
     note_text = "Orientering om godkendelse af udrejse er sendt til anmelder"
     eflyt_case.add_note(webdriver, note_text)
+    oc.log_trace("Case approved and note added.")
 
 
 if __name__ == "__main__":
